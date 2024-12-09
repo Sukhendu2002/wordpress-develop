79397020c8f805b804db1d228823dbbc7ba59a81
Sukhendu Sekhar Guria <sukhendu.guria@rtcamp.com>
2024-12-09 15:50:29 +0530

    Update WP_PHPMailer class for improved internationalization and error message translations

--- tests/phpunit/data/pomo/simple.l10n.php
+++ tests/phpunit/data/pomo/simple.l10n.php
@@ -1,3 +1,5 @@
 <?php
 return ['project-id-version'=>'WordPress 2.6-bleeding','report-msgid-bugs-to'=>'wp-polyglots@lists.automattic.com','messages'=>['baba'=>'dyado','kuku
-ruku'=>'yes']];
+ruku'=>'yes',
+'You must provide at least one recipient email address.' => 'Vous devez fournir au moins une adresse e-mail de destinataire.'
+]];
